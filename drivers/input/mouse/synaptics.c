33e67f60ea9f19814109f88a812d2ed11c84f75f
Mantas Mikulėnas <grawity@gmail.com>
2018-12-21 01:04:26 -0800

    Input: synaptics - enable SMBus for HP EliteBook 840 G4
    [ Upstream commit 7a71712293ba303aad928f580b89addb0be2892e ]
    
    dmesg reports that "Your touchpad (PNP: SYN3052 SYN0100 SYN0002 PNP0f13)
    says it can support a different bus."
    
    I've tested the offered psmouse.synaptics_intertouch=1 with 4.18.x and
    4.19.x and it seems to work well. No problems seen with suspend/resume.
    
    Also, it appears that RMI/SMBus mode is actually required for 3-4 finger
    multitouch gestures to work -- otherwise they are not reported at all.
    
    Information from dmesg in both modes:
    
      psmouse serio3: synaptics: Touchpad model: 1, fw: 8.2, id: 0x1e2b1,
          caps: 0xf00123/0x840300/0x2e800/0x0, board id: 3139, fw id: 2000742
    
      psmouse serio3: synaptics: Trying to set up SMBus access
      rmi4_smbus 6-002c: registering SMbus-connected sensor
      rmi4_f01 rmi4-00.fn01: found RMI device,
          manufacturer: Synaptics, product: TM3139-001, fw id: 2000742
    
    Signed-off-by: Mantas Mikulėnas <grawity@gmail.com>
    Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -184,6 +184,7 @@ static const char * const smbus_pnp_ids[] = {
 	"LEN200f", /* T450s */
 	"SYN3257", /* HP Envy 13-ad105ng */
 	"LEN2044", /* L470  */
+	"SYN3052", /* HP EliteBook 840 G4 */
 	"SYN3221", /* HP 15-ay000 */
 	NULL
 };
