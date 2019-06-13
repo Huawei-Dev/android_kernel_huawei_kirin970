4e848cc9b069a5f338f7bc220515ed6b3c4c4824
Greg Hackmann <ghackmann@google.com>
2019-06-13 15:34:07 -0700

    UPSTREAM: dma-buf: add DMA_BUF_SET_NAME ioctls
    This patch adds complimentary DMA_BUF_SET_NAME  ioctls, which lets
    userspace processes attach a free-form name to each buffer.
    
    This information can be extremely helpful for tracking and accounting
    shared buffers.  For example, on Android, we know what each buffer will
    be used for at allocation time: GL, multimedia, camera, etc.  The
    userspace allocator can use DMA_BUF_SET_NAME to associate that
    information with the buffer, so we can later give developers a
    breakdown of how much memory they're allocating for graphics, camera,
    etc.
    
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190613223408.139221-3-fengc@google.com
    (cherry picked from commit bb2bb903042517b8fb17b2bc21e00512f2dcac01)
    
    Bug: 135613055
    Test: run dmabufinfo_test.cpp and check the name is showed up in
          /proc/pid/fd
    
    Change-Id: Ib7f227b5e730c7ecfade14ad596ba3889eab1b0b

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -48,6 +48,22 @@ struct dma_buf_list {
 
 static struct dma_buf_list db_list;
 
+static char *dmabuffs_dname(struct dentry *dentry, char *buffer, int buflen)
+{
+	struct dma_buf *dmabuf;
+	char name[DMA_BUF_NAME_LEN];
+	size_t ret = 0;
+
+	dmabuf = dentry->d_fsdata;
+	mutex_lock(&dmabuf->lock);
+	if (dmabuf->name)
+		ret = strlcpy(name, dmabuf->name, DMA_BUF_NAME_LEN);
+	mutex_unlock(&dmabuf->lock);
+
+	return dynamic_dname(dentry, buffer, buflen, "/%s:%s",
+			     dentry->d_name.name, ret > 0 ? name : "");
+}
+
 static void dma_buf_release(struct dentry *dentry)
 {
 	struct dma_buf *dmabuf;
@@ -85,7 +101,7 @@ static void dma_buf_release(struct dentry *dentry)
 }
 
 static const struct dentry_operations dma_buf_dentry_ops = {
-	.d_dname = simple_dname,
+	.d_dname = dmabuffs_dname,
 	.d_release = dma_buf_release,
 };
 
@@ -299,33 +315,40 @@ out:
 	return events;
 }
 
+/**
+ * dma_buf_set_name - Set a name to a specific dma_buf to track the usage.
+ * The name of the dma-buf buffer can only be set when the dma-buf is not
+ * attached to any devices. It could theoritically support changing the
+ * name of the dma-buf if the same piece of memory is used for multiple
+ * purpose between different devices.
+ *
+ * @dmabuf [in]     dmabuf buffer that will be renamed.
+ * @buf:   [in]     A piece of userspace memory that contains the name of
+ *                  the dma-buf.
+ *
+ * Returns 0 on success. If the dma-buf buffer is already attached to
+ * devices, return -EBUSY.
+ *
+ */
 static long dma_buf_set_name(struct dma_buf *dmabuf, const char __user *buf)
 {
 	char *name = strndup_user(buf, DMA_BUF_NAME_LEN);
+	long ret = 0;
 
 	if (IS_ERR(name))
 		return PTR_ERR(name);
 
 	mutex_lock(&dmabuf->lock);
+	if (!list_empty(&dmabuf->attachments)) {
+		ret = -EBUSY;
+		kfree(name);
+		goto out_unlock;
+	}
 	kfree(dmabuf->name);
 	dmabuf->name = name;
+
+out_unlock:
 	mutex_unlock(&dmabuf->lock);
-
-	return 0;
-}
-
-static long dma_buf_get_name(struct dma_buf *dmabuf, char __user *buf)
-{
-	const char *name = "";
-	long ret = 0;
-
-	mutex_lock(&dmabuf->lock);
-	if (dmabuf->name)
-		name = dmabuf->name;
-	if (copy_to_user(buf, name, strlen(name) + 1))
-		ret = -EFAULT;
-	mutex_unlock(&dmabuf->lock);
-
 	return ret;
 }
 
@@ -370,9 +393,6 @@ static long dma_buf_ioctl(struct file *file,
 
 	case DMA_BUF_SET_NAME:
 		return dma_buf_set_name(dmabuf, (const char __user *)arg);
-
-	case DMA_BUF_GET_NAME:
-		return dma_buf_get_name(dmabuf, (char __user *)arg);
 
 	default:
 		return -ENOTTY;
