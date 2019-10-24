14533365d94893122b357084af19af76d276a0f7
Satya Tangirala <satyat@google.com>
2019-10-24 14:44:27 -0700

    BACKPORT: FROMLIST: scsi: ufs: UFS crypto API
    Introduce functions to manipulate UFS inline encryption hardware
    in line with the JEDEC UFSHCI v2.1 specification and to work with the
    block keyslot manager.
    
    Bug: 137270441
    Test: tested as series; see Ie1b77f7615d6a7a60fdc9105c7ab2200d17636a8
    Change-Id: I98d2512858a231f11a6bf868198446378d3aa491
    Signed-off-by: Satya Tangirala <satyat@google.com>
    Link: https://lore.kernel.org/linux-fscrypt/20191028072032.6911-6-satyat@google.com/

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -759,6 +759,11 @@ struct ufs_stats {
  * @urgent_bkops_lvl: keeps track of urgent bkops level for device
  * @is_urgent_bkops_lvl_checked: keeps track if the urgent bkops level for
  *  device is known or not.
+ * @crypto_capabilities: Content of crypto capabilities register (0x100)
+ * @crypto_cap_array: Array of crypto capabilities
+ * @crypto_cfg_register: Start of the crypto cfg array
+ * @crypto_cfgs: Array of crypto configurations (i.e. config for each slot)
+ * @ksm: the keyslot manager tied to this hba
  */
 struct ufs_hba {
 	void __iomem *mmio_base;
@@ -1195,6 +1200,15 @@ struct ufs_hba {
 	u64 wr_find_node_time;
 	u64 write_cnt;
 #endif
+
+#ifdef CONFIG_SCSI_UFS_CRYPTO
+	/* crypto */
+	union ufs_crypto_capabilities crypto_capabilities;
+	union ufs_crypto_cap_entry *crypto_cap_array;
+	u32 crypto_cfg_register;
+	union ufs_crypto_cfg_entry *crypto_cfgs;
+	struct keyslot_manager *ksm;
+#endif /* CONFIG_SCSI_UFS_CRYPTO */
 };
 
 #ifdef CONFIG_SCSI_UFS_SYNCCACHE_RECORD
