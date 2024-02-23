24cfda1fa677e5e92de316b3738a01d46e4b7d25
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-23 13:52:09 +0100

    fs: Align file struct to 8 bytes
    This improves hackbench results by about 5% on average.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Neebe3289 <neebexd@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -911,8 +911,7 @@ struct file {
 #ifdef CONFIG_HMFS_FS
 	unsigned char hm_fsync_flag;
 #endif
-}
-  __attribute__((aligned(4)));	/* lest something weird decides that 2 is OK */
+} __attribute__((aligned(8)));	/* lest something weird decides that 2 is OK */
 
 struct file_handle {
 	__u32 handle_bytes;
