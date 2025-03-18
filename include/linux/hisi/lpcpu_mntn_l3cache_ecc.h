921565817d080061ecc495ea8bb0b816a2ad4855
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-18 20:00:39 +0100

    drop unused flag CONFIG_HISI_L3CACHE_ECC

--- include/linux/hisi/lpcpu_mntn_l3cache_ecc.h
+++ include/linux/hisi/lpcpu_mntn_l3cache_ecc.h
@@ -20,13 +20,9 @@
 
 #include "../../../drivers/hisi/mntn/mntn_l3cache_ecc.h"
 
-#ifdef CONFIG_HISI_L3CACHE_ECC
-enum serr_type l3cache_ecc_get_status(u64 *err1_status, u64 *err1_misc0);
-#else
 static inline enum serr_type l3cache_ecc_get_status(u64 *err1_status,
 						    u64 *err1_misc0)
 {
 	return NO_EEROR;
 }
-#endif
 #endif /* __LPCPU_MNTN_L3CACHE_ECC_H__ */
