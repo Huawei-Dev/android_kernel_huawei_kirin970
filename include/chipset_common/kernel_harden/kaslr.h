6a4bd5dd8882c8d7a9a910a9ff6d494df673685e
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-23 10:31:20 +0100

    drop unused flag CONFIG_HUAWEI_KERNEL_STACK_NX

--- include/chipset_common/kernel_harden/kaslr.h
+++ include/chipset_common/kernel_harden/kaslr.h
@@ -1,19 +1,10 @@
 #ifndef KASLR_H
 #define KASLR_H
 
-#ifndef CONFIG_HUAWEI_KERNEL_STACK_NX
 #define set_init_stack_nx(x)
 #define set_task_stack_nx(x)
-#else
-extern void __init set_init_stack_nx(struct thread_info *ti);
-extern void set_task_stack_nx(struct thread_info *ti);
-#endif
 
-#ifndef CONFIG_HUAWEI_KERNEL_STACK_NX
 #define set_init_stack_nx(x)
-#else
-extern void __init set_init_stack_nx(struct thread_info *ti);
-#endif
 
 #ifndef CONFIG_HUAWEI_KERNEL_MODULE_RANDOMIZE
 #define get_module_load_offset()
