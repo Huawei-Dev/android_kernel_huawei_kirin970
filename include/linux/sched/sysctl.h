9c1d1e4005adf912e0844131314ec4e09d3ddbce
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-27 18:14:00 +0100

    drop unused flag CONFIG_RT_CAS

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -48,9 +48,6 @@ extern unsigned int sysctl_sched_force_upmigrate_duration;
 #endif
 #endif
 
-#ifdef CONFIG_RT_CAS
-extern unsigned int sysctl_sched_enable_rt_cas;
-#endif
 #ifdef CONFIG_RT_ACTIVE_LB
 extern unsigned int sysctl_sched_enable_rt_active_lb;
 #endif
