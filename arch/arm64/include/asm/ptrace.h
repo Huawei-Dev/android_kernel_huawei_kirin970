16d9296cc5ba2817e9992257f385f6fc62d32c05
Huawei_Dev <surdu_petru@yahoo.com>
2025-06-12 18:27:21 +0200

    arm64: drop unused flag CONFIG_HKIP_ADDR_LIMIT_PROTECTION

--- arch/arm64/include/asm/ptrace.h
+++ arch/arm64/include/asm/ptrace.h
@@ -192,11 +192,7 @@ struct pt_regs {
 #endif
 
 	u64 orig_addr_limit;
-#ifdef CONFIG_HKIP_ADDR_LIMIT_PROTECTION
-	u32 orig_addr_limit_hkip[2];
-#else
 	u64 unused;
-#endif
 	// maintain 16 byte alignment
 	u64 stackframe[2];
 };
