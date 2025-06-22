f0b5ace01dca030d14c6e8748941b0eb28c758c4
Huawei_Dev <surdu_petru@yahoo.com>
2025-06-22 11:17:56 +0200

    defconfig: drop CONFIG_HUAWEI_HIDESYMS & CONFIG_HW_SLUB_SANITIZE

--- mm/slub.c
+++ mm/slub.c
@@ -38,7 +38,6 @@
 
 #include <linux/hisi/rdr_hisi_ap_hook.h>
 #include <trace/events/kmem.h>
-#include <chipset_common/security/saudit.h>
 #include "internal.h"
 
 #ifdef CONFIG_HW_SLUB_DF
@@ -245,18 +244,6 @@ static inline void stat(const struct kmem_cache *s, enum stat_item si)
 #endif
 }
 
-#ifdef CONFIG_HW_SLUB_SANITIZE
-static inline bool has_sanitize(struct kmem_cache *s)
-{
-#ifdef CONFIG_HW_SLUB_DF
-	return false;
-#else
-	return (s->flags & SLAB_CLEAR) &&
-		!(s->flags & (SLAB_TYPESAFE_BY_RCU | SLAB_POISON));
-#endif
-}
-#endif
-
 /********************************************************************
  * 			Core slab cache functions
  *******************************************************************/
@@ -322,19 +309,6 @@ static inline void set_freepointer(struct kmem_cache *s, void *object, void *fp)
 {
 	unsigned long freeptr_addr = (unsigned long)object + s->offset;
 
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(object == fp)) {
-#ifdef CONFIG_HW_SLUB_DF
-		set_harden_double_free_check_flags(true);
-#else
-		s->flags |= SLAB_CLEAR;
-#endif
-		WARN_ON(1);
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=light,kmem_cache_name=%s", s->name);
-		return;
-	}
-#endif
 	*(void **)freeptr_addr = freelist_ptr(s, fp, freeptr_addr);
 }
 
@@ -378,8 +352,6 @@ static inline bool hw_check_canary(struct kmem_cache *s, void *object, unsigned 
 	canary = object + sizeof(void *);
 
 	if (*canary == hw_get_canary_value(canary, value)) {
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=harden,kmem_cache_name=%s", s->name);
 		return false;
 	}
 	return true;
@@ -396,8 +368,6 @@ static inline bool hw_check_and_set_canary(struct kmem_cache *s, void *object,
 	canary = object + sizeof(void *);
 
 	if (*canary == hw_get_canary_value(canary, value)) {
-		saudit_log(DOUBLE_FREE, STP_RISK, SAUDIT_ASYNC,
-			"type=harden,kmem_cache_name=%s", s->name);
 		return false;
 	}
 	*canary = hw_get_canary_value(canary, value);
@@ -3031,10 +3001,6 @@ static void __slab_free(struct kmem_cache *s, struct page *page,
 		prior = page->freelist;
 		counters = page->counters;
 		set_freepointer(s, tail, prior);
-#ifdef CONFIG_HW_SLUB_SANITIZE
-		if (unlikely(get_freepointer(s, tail) != prior))
-			return;
-#endif
 		new.counters = counters;
 		was_frozen = new.frozen;
 		new.inuse -= cnt;
@@ -3145,21 +3111,6 @@ static __always_inline void do_slab_free(struct kmem_cache *s,
 	void *tail_obj = tail ? : head;
 	struct kmem_cache_cpu *c;
 	unsigned long tid;
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(has_sanitize(s))) {
-		int offset = s->offset ? 0 : sizeof(void *);
-		void *x = head;
-
-		while (1) {
-			memset(x + offset, 0, s->object_size - offset);
-			if (s->ctor)
-				s->ctor(x);
-			if (x == tail_obj)
-				break;
-			x = get_freepointer(s, x);
-		}
-	}
-#endif
 
 #ifdef CONFIG_HW_SLUB_DF
 	if (unlikely(s->flags & SLAB_DOUBLEFREE_CHECK) &&
@@ -3184,10 +3135,6 @@ redo:
 
 	if (likely(page == c->page)) {
 		set_freepointer(s, tail_obj, c->freelist);
-#ifdef CONFIG_HW_SLUB_SANITIZE
-	if (unlikely(get_freepointer(s, tail_obj) != c->freelist))
-		return;
-#endif
 		if (unlikely(!this_cpu_cmpxchg_double(
 				s->cpu_slab->freelist, s->cpu_slab->tid,
 				c->freelist, tid,
@@ -4142,10 +4089,6 @@ err:
 	pr_err("ptr = %pK, page = %pK, n = %lu\n", ptr, page, n);
 	pr_err("page_addr = %pK, kmem_cache = %pK, size = %d, object= %lu, red_left_pad = %d",
 		page_address(page), s, s->size, object_size, s->red_left_pad);
-
-	saudit_log(USERCOPY, STP_RISK, 0,
-		"msg=kernel memory r/w attempt detected from/to %pK (%s) (%lu bytes),",
-		ptr, s->name, n);
 
 	BUG();
 }
